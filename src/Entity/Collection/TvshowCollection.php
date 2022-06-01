5cb8026c0cca3f6fd4614b61b97760f24f15779a
Alexis Hermant <alexis.hermant@etudiant.univ-reims.fr>
2022-06-01 11:09:31 +0200

    ajout fonction findAll()

--- src/Entity/Collection/TvshowCollection.php
+++ src/Entity/Collection/TvshowCollection.php
@@ -4,8 +4,20 @@ declare(strict_types=1);
 namespace Entity\Collection;
 
 use Entity\Tvshow;
+use Database\MyPdo;
+use PDO;
 
 class TvshowCollection
 {
-
+    public static function findAll(): array
+    {
+        $stmt = MyPDO::getInstance()->prepare(
+            <<<'SQL'
+            SELECT *
+            FROM tvshow;
+            SQL
+        );
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, "Entity\Tvshow");
+    }
 }
