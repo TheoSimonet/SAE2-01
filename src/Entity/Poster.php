01aae5a9848e4f902a4f1b70980a3c24f8565b27
Alexis Hermant <alexis.hermant@etudiant.univ-reims.fr>
2022-06-01 10:49:54 +0200

    ajout de la class Poster

---
+++ src/Entity/Poster.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Entity;
+
+class Poster
+{
+    private int $id;
+    private string $jpeg;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getJpeg(): string
+    {
+        return $this->jpeg;
+    }
+}
