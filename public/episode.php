616c817ba860fc9394cf5d2da1b2c82c0cdbd0fa
Alexis Hermant <alexis.hermant@etudiant.univ-reims.fr>
2022-06-02 17:01:12 +0200

    ajout lien pour retour a la page de la série

--- public/episode.php
+++ public/episode.php
@@ -12,10 +12,12 @@ $web = new AppWebPage();
 
 $season = Season::findById($seasonId);
 $tvshow = \Entity\Tvshow::findById($season->getTvShowId());
-$web->setTitle("Séries TV : " . $tvshow->getName() . $season->getName());
+$web->setTitle("Séries TV : " . $tvshow->getName() ." ". $season->getName());
+$web->appendContent("<p class='encadretitle'><img src=poster.php?posterId={$season->getPosterId()}>{$web::escapeString($season->getName())}<a href='season.php?tvshowId={$tvshow->getId()}'>{$web::escapeString($tvshow->getName())}</a></p>");
 
 foreach (EpisodeCollection::findBySeasonId($seasonId) as $episode) {
     $titre = \Html\WebPage::escapeString($episode->getName());
-    $web->appendContent("<p>$titre</p>");
+    $web->appendContent("<div class='encadreepisode'><p class='titreepisode'>$titre</p>");
+    $web->appendContent("<p class='desc'>{$web::escapeString($episode->getOverview())}</p>");
 }
 echo $web->toHTML();
