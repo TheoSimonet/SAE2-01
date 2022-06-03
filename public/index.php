dcd6213c4d5b08d2f40637ed71254b2976e2aafd
Alexis Hermant <alexis.hermant@etudiant.univ-reims.fr>
2022-06-03 10:31:58 +0200

    ajout du filtrage du genre

--- public/index.php
+++ public/index.php
@@ -11,7 +11,34 @@ $webPage = new AppWebPage();
 $webPage->setTitle('Séries TV');
 $webPage->appendCssUrl("/css/style.css");
 
-$series = \Entity\Collection\TvshowCollection::findAll();
+if (isset($_GET["Filtre"]) && ctype_digit($_GET["Filtre"]) && $_GET["Filtre"] != '0') {
+    $series = \Entity\Collection\GenreCollection::findAll((int)$_GET["Filtre"]);
+} else {
+    $series = \Entity\Collection\TvshowCollection::findAll();
+}
+
+$header = <<<HTML
+    <form action="index.php" method="get">
+        FILTRE :
+        <select name="Filtre" size="1">
+            <option value="0">Tous les genres</option>
+            <option value="1">Action & Aventure</option>
+            <option value="2">Crime</option>
+            <option value="3">Drame</option>
+            <option value="4">Comédie</option>
+            <option value="5">Science-Fiction & Fantastique</option>
+            <option value="6">Mystère</option>
+            <option value="7">Western</option>
+            <option value="8">War & Politics</option>
+            <option value="9">Familial</option>
+            <option value="10">Animation</option>
+            <option value="11">Romance</option>
+        </select>
+        <button type="submit"> Appliquer </button>
+    </form>
+HTML;
+
+$webPage->appendContent($header);
 
 $webPage->appendContent(
     <<<HTML
@@ -25,7 +52,7 @@ foreach ($series as $index => $tvshow) {
     } else {
         $src = "poster.php?posterId={$tvshow->getPosterId()}";
     }
-    
+
     $webPage->appendContent(
         <<<HTML
     <p class="encadre"> <img class="poster" src='{$src}'"> <a href="season.php?tvshowId={$tvshow->getId()}">{$webPage::escapeString($tvshow->getName())}.{$webPage::escapeString($tvshow->getOverview())}</a></p>
